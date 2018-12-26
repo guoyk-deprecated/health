e0785d536fb656b10a3d06fde89333e994fa0953
canhead <hi@canhead.xyz>
2018-12-26 17:55:01 +0800

    complete health middleware

---
+++ health_test.go
@@ -0,0 +1,65 @@
+package health
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/labstack/echo"
+)
+
+type testResource int
+
+func (t *testResource) HealthCheck() error {
+	*t++
+	if *t%2 == 0 {
+		return nil
+	}
+	return errors.New("BAD")
+}
+
+func TestHealth(t *testing.T) {
+	var i testResource
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/.well-known/health", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	handler := func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	m := New(&i)
+	h := m(handler)
+	h(c)
+
+	assert.Equal(t, "BAD", rec.Body.String())
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	handler = func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	m = New(&i)
+	h = m(handler)
+	h(c)
+
+	assert.Equal(t, "test", rec.Body.String())
+	assert.Equal(t, http.StatusOK, rec.Code)
+
+	req = httptest.NewRequest(http.MethodGet, "/.well-known/health", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	handler = func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	m = New(&i)
+	h = m(handler)
+	h(c)
+
+	assert.Equal(t, "OK", rec.Body.String())
+	assert.Equal(t, http.StatusOK, rec.Code)
+}
