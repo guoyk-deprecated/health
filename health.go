54c209dc5eb594408dffdfe6794ab4ba4b92c9b1
canhead <hi@canhead.xyz>
2019-03-21 14:35:51 +0800

    using GO111MODULE

--- health.go
+++ health.go
@@ -3,7 +3,7 @@ package health
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 // Resource something that need be health checked
--- health_test.go
+++ health_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 type testResource int
